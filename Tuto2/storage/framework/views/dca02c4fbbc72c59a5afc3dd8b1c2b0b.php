c418856c299586269c4079705f2984787a2acca3
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2024-10-30 09:11:07 +0000

    ttuto4

---
+++ Tuto2/storage/framework/views/dca02c4fbbc72c59a5afc3dd8b1c2b0b.php
@@ -0,0 +1,7 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <h1>Bienvenue sur mon site !</h1>
+    <p>Ceci est la page d'accueil.</p>
+<?php $__env->stopSection(); ?> 
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Solicode\Tuto1\resources\views/welcomeApp.blade.php ENDPATH**/ ?>
