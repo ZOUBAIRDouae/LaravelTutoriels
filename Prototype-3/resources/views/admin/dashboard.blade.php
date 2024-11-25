09307c18de26ea5f6c42052e0b51f96ab358d928
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2024-11-25 16:13:35 +0000

    prototype3

---
+++ Prototype-3/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Liste des Articles</h1>
+                </div>
+                <div class="card">
+                    <div class="card-header col-md-12">
+                        <a href="/articles/create" class="btn btn-primary">Crée un nouvel Article</a>
+                    </div>
+                    <div class="card-body col-md-12">
+                        <table>
+                            @foreach ($articles as $article)
+                            <tr>
+                                <td>{{$article['title']}}</td>
+                                <td>{{$article['content']}}</td>
+                                <td><a href="/articles/{{$article['id']}}" class="btn btn-sm btn-secondary ">Afficher</a></td>
+                            </tr>
+                            @endforeach
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@include('partials.flash-message')
+
+
+
+
+
+        
+
+
