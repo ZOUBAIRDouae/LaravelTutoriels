93db3ea175e7f208ce0114b8e2206c026809a93d
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2025-01-22 13:55:32 +0000

    ajax

---
+++ AjaxAtelier/app/Http/Controllers/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index',compact('products'));
+    }
+
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+        ]);
+
+        if( $validator->fails())
+        {
+            return response()->json([
+                'status'=>400,
+                'errors' => $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $product = new Product();
+            $product->name = $request->name;
+            $product->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'created successfuly'
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
